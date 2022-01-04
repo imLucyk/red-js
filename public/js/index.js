608da0193487c04778210ce2c6b8f340d4dd2fbd
imLucyk <88808248+imLucyk@users.noreply.github.com>
2022-01-04 21:02:03 +0900

    [#8] firebase 이메일 로그인 & 로그인에 따른 메뉴 보여주기

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,16 @@
 firebase.auth().onAuthStateChanged(function(firebaseUser) {
   console.log(firebaseUser);
   if (firebaseUser) {
-    // document.getElementById('login-display').innerHTML = firebaseUser.email + ' 반가워요!';
+    document.getElementById('guest').style.display = 'none';
+    document.getElementById('login').style.display = 'none';
+    document.getElementById('hello').style.display = 'block';
+    document.getElementById('logout').style.display = 'block';
+    document.getElementById('name').innerHTML = firebaseUser.displayName || 'guest';
   } else {
-    // document.getElementById('login-display').innerHTML = '';
+    document.getElementById('guest').style.display = 'block';
+    document.getElementById('login').style.display = 'block';
+    document.getElementById('hello').style.display = 'none';
+    document.getElementById('logout').style.display = 'none';
   }
 });
 
@@ -14,4 +21,22 @@ const googleLogout = function() {
 const googleLogin = function() {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider);
+};
+
+const emailSignup = function() {
+  const email = 'guest@red-js.web.app'
+  const password = 'guestguest'
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    console.error(error);
+    alert(error.message);
+  });
+};
+
+const emailSignin = function() {
+  const email = 'guest@red-js.web.app'
+  const password = 'guestguest'
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    console.error(error);
+    alert(error.message);
+  });
 };
